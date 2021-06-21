fc19a7b62c8028338730cef0af07b4eeacc5ddf2
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2021-06-21 21:36:59 +0200

    mutate: add test case for testingg the test environment

---
+++ plugin/mutate/testdata/bug_test_inf_loop.cpp
@@ -0,0 +1,14 @@
+/// @copyright Boost License 1.0, http://boost.org/LICENSE_1_0.txt
+/// @date 2021
+/// @author Joakim Brännström (joakim.brannstrom@gmx.com)
+
+#include <cstdint>
+
+int main(int argc, char** argv) {
+    uint32_t i = 0;
+    while (i < 10) {
+        i = i + 1;
+    }
+
+    return 0;
+}
