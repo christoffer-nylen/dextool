58c5ca827b57f4648f4fa16bdac38c2d05d62c2b
Christoffer Nylén <5559216+christoffer-nylen@users.noreply.github.com>
2026-03-31 21:56:14 -0300

    Inline suffix character among check

--- plugin/mutate/source/dextool/plugin/mutate/backend/analyze/pass_filter.d
+++ plugin/mutate/source/dextool/plugin/mutate/backend/analyze/pass_filter.d
@@ -147,14 +147,13 @@ bool isUndesiredCppPattern(Blob file, Offset o, const(ubyte)[] mutant) {
 }
 
 bool isEquivalentZeroMutant(const(ubyte)[] original, const(ubyte)[] mutant) {
-    static immutable ubyte[8] integerLiteralSuffixChars = ['u', 'U', 'l', 'L', 'v', 'V', 'z', 'Z'];
-
     if (original.length < 2 || mutant != ['0']) {
         return false;
     }
 
     size_t literalEnd = original.length;
-    while (literalEnd > 0 && original[literalEnd - 1].among(integerLiteralSuffixChars[])) {
+    while (literalEnd > 0
+            && original[literalEnd - 1].among('u', 'U', 'l', 'L', 'v', 'V', 'z', 'Z')) {
         --literalEnd;
     }
 
